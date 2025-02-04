3e0679934bc1ad67281b758b0f886637f3e8979c
Tirth Patel <tirthpatel61@outlook.com>
2025-02-04 10:11:29 +0530

    Spawn asteroids by creating `AsteroidField` object

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pygame
 from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
@@ -18,9 +19,11 @@ def main():
     drawable = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
